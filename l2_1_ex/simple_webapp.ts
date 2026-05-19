495b06cfa54815d223821916a0d7c1f70fec7c8c
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2026-05-19 12:26:18 +0800

    updated

---
+++ l2_1_ex/simple_webapp.ts
@@ -0,0 +1,22 @@
+import * as http from 'http';
+
+const webAppServer = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  if (req.url === "/") {
+    res.write(`
+        <html>
+            <head><title>Welcome</title></head>
+            <body>Welcome to 50.003!</body>
+        </html>`);
+    res.end();
+  }
+  else {
+    res.write(`
+        <html>
+            <head><title>Error</title></head>
+            <body>Page not found</body>
+        </html>`);
+    res.end();
+  }
+});
+
+webAppServer.listen(3000);
